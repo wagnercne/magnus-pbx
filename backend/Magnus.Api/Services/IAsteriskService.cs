3593850d90c911caedac7b30cb592d06dba1ea74
WagnerCNE <wagnercne@yahoo.com.br>
2026-02-20 18:12:30 -0300

    chore(backend): remove legacy Magnus.Api path after migration to Magnus.Pbx

--- backend/Magnus.Api/Services/IAsteriskService.cs
+++
@@ -1,7 +0,0 @@
-namespace Magnus.Pbx.Services;
-
-public interface IAsteriskService
-{
-    Task<bool> OriginateCallAsync(string extension, string context, string application, string data);
-    Task<string?> ExecuteCommandAsync(string command);
-}
